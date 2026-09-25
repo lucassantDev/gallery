b88936c0b73bd1b2d21974d1146ad4b3b8ef0f11
lucassant <lucasfdesantana095@gmail.com>
2026-09-25 12:12:35 -0300

    feat: adicionando input para subir arquivos

--- src/components/FirstSection/togglePicture.jsx
+++ src/components/FirstSection/togglePicture.jsx
@@ -1,13 +1,18 @@
-import {Plus} from "lucide-react"
+import {ImagePlus} from "lucide-react"
 
 export default function togglePicture() {
     return    (
         <>
-            <div className="group border border-gray-400 w-xl h-2/3 flex items-center justify-center transition duration-300 cursor-pointer hover:border-gray-600">
-                <Plus
-                    size={96}
-                    className="text-gray-400 transition duration-300 group-hover:text-gray-600"
-                />
+            <div className="border border-gray-200 w-xl h-2/3 flex flex-col gap-6 items-center justify-center transition duration-300  hover:shadow-md">
+                    <label>
+                        <ImagePlus
+                            size={44}
+                            className="text-gray-400 transition duration-300 cursor-pointer hover:text-gray-600"
+                        
+                    />
+                    <input type="file" className="hidden" />
+                    </label>
+                    <p className="font-semibold text-gray-400">Escolha seu arquivo</p>                    
             </div>
         </>
     )
